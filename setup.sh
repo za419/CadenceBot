baba5b86d158d7d513917b7a38de7e01260e3ef4
Ryan Hodin <ryan951357@gmail.com>
2017-10-28 01:53:06 -0500

    Add setup script
    This script will probably reduce the number of times I try to start the
    bot a package short. Probably.
    
    Assuming I use it correctly, that is.
    
    At least it makes it easier to deploy new bot instances...?

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Run me whenever you update the server
+# Update me whenever the list of npm installs we need to run changes
+
+npm install --save discord.js
+npm install --save node-fetch
+npm install --save request
+npm install --save node-opus
