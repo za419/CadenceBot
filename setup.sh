4097b13eb47c6fd440756de25950e04d278ac7ae
Ryan Hodin <ryan951357@gmail.com>
2017-12-30 01:16:19 -0800

    Add object.defaults dependency

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # Update me whenever the list of npm installs we need to run changes
 
 npm install --save js-logging
+npm install --save object.defaults
 npm install --save discord.js
 npm install --save zlib-sync
 npm install --save libsodium-wrappers@^0.5.4
