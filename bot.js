d277db79a7f29c91d5d38a394b8aecbf956a54d2
Ryan Hodin <ryan951357@gmail.com>
2020-02-04 22:26:04 -0600

    Fix array merging

--- bot.js
+++ bot.js
@@ -19,11 +19,13 @@ function recursiveDefault(obj, def) {
     var keys=Object.keys(obj);
     for (var i=0; i<keys.length; ++i) {
         var key=keys[i];
-        if (obj[key] instanceof Object && def[key] instanceof Object) {
-            recursiveDefault(obj[key], def[key]);
+        if (obj[key] instanceof Array || def[key] instanceof Array) {
+            if (obj[key] instanceof Array && def[key] instanceof Array) {
+                obj[key]=obj[key].concat(def[key]);
+            }
         }
-        else if (obj[key] instanceof Array && def[key] instanceof Array) {
-            obj[key]=obj[key].concat(def[key]);
+        else if (obj[key] instanceof Object && def[key] instanceof Object) {
+            recursiveDefault(obj[key], def[key]);
         }
     }
 }
