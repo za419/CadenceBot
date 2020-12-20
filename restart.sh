1229b1e48a9ae6d925677d74d0e9af99605db2a5
Ryan Hodin <ryan951357@gmail.com>
2020-12-19 19:37:23 -0600

    Add attempt at sudo if needed to restart

--- restart.sh
+++ restart.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 pushd "$(dirname "$(readlink -f "$0")")" > /dev/null
-pkill node
+if ! pkill node; then
+    # Try sudo before things break
+    sudo pkill node
+fi
 ./maillog.sh 2> /dev/null
 nohup node bot.js &>CadenceBot.log &
 popd > /dev/null
