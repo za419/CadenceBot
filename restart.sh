dfb6855e15c8542fec70b7f2c9154d0d452e0dc0
Ryan Hodin <ryan951357@gmail.com>
2020-12-19 19:43:23 -0600

    Add note on use of sudo

--- restart.sh
+++ restart.sh
@@ -3,6 +3,7 @@
 pushd "$(dirname "$(readlink -f "$0")")" > /dev/null
 if ! pkill node 2>/dev/null; then
     # Try sudo before things break
+    echo "Trying sudo to kill old running instance (probably started by rc.local)"
     sudo pkill node
 fi
 ./maillog.sh 2> /dev/null
